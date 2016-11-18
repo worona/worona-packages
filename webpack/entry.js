f8760985f54cc56553fc228a09be0a86ff181d8a
Luis Herranz <luisherranz@gmail.com>
2016-11-18 13:27:17 +0100

    Add default properties to entry point export.

--- webpack/entry.js
+++ webpack/entry.js
@@ -1,7 +1,23 @@
 require('babel-polyfill');
 var pkg = require('../../../src/dashboard').default;
+var pkgJson = require('../../../package.json');
 var packageDevelopment = require('worona-deps').packageDevelopment;
 
-packageDevelopment(pkg);
+packageDevelopment(Object.assign(pkg, {
+  name: pkgJson.name,
+  namespace: pkgJson.worona.namespace,
+  woronaInfo: {
+    name: pkgJson.name,
+    id: pkgJson.name,
+    namespace: pkgJson.worona.namespace,
+    niceName: pkgJson.worona.niceName,
+    type: pkgJson.worona.type,
+    menu: {
+      category: 'Development',
+      order: 10,
+    },
+    services: pkgJson.worona.services,
+  }
+}));
 
-console.log('Development package loaded!');
+console.log('Development package ' + pkgJson.name + ' loaded!');
