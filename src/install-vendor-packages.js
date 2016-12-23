0a181cfe34cd36c8a392fcc874d633fa67d42f4c
Luis Herranz <luisherranz@gmail.com>
2016-12-23 14:28:45 +0100

    Switch to vendors.

--- src/install-vendor-packages.js
+++ src/install-vendor-packages.js
@@ -3,11 +3,11 @@ import { gt } from 'semver';
 import { getPackageVersion } from './utils';
 
 export default async ({ service, packageJson }) => {
-  const remoteVersion = await getPackageVersion(`core-${service}-worona`);
-  const localVersion = packageJson.devDependencies[`core-${service}-worona`] || '0.0.0';
+  const remoteVersion = await getPackageVersion(`vendors-${service}-worona`);
+  const localVersion = packageJson.devDependencies[`vendors-${service}-worona`] || '0.0.0';
   if (gt(remoteVersion, localVersion)) {
-    console.log(`\nThere is a new version of core-${service}-worona. Updating...`);
-    await spawn('npm', ['install', '--save-dev', '--save-exact', `core-${service}-worona@${remoteVersion}`],
+    console.log(`\nThere is a new version of vendors-${service}-worona. Updating...`);
+    await spawn('npm', ['install', '--save-dev', '--save-exact', `vendors-${service}-worona@${remoteVersion}`],
       { stdio: 'inherit' });
     console.log(`Updating finished.\n`);
   }
