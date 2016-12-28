22d6aa2fe5dfb297ba30246385f8e05596741933
Luis Herranz <luisherranz@gmail.com>
2016-12-28 16:35:48 +0100

    Fix service load on entry point.

--- webpack/entry.js
+++ webpack/entry.js
@@ -1,12 +1,13 @@
 /* eslint-disable */
+var service = process.env.SERVICE_ENV;
 require('babel-polyfill');
-var pkg = require('../../../src/' + process.env.SERVICE_ENV);
+var pkg = require('../../../src/' + service + '/index.js');
 var pkgJson = require('../../../package.json');
 var packageDevelopment = require('worona-deps').packageDevelopment;
 
 packageDevelopment(Object.assign(
   pkg,
   { woronaInfo: Object.assign(pkgJson.worona, { name: pkgJson.name }) }
-), pkgJson.name, pkgJson.worona.dashboard.namespace);
+), pkgJson.name, pkgJson.worona[service].namespace);
 
 console.log('Development package ' + pkgJson.name + ' loaded!');
